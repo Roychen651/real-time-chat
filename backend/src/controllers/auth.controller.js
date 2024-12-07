d3befc0c99b72ff733dd264d3d37a73011d9648d
roychen651 <roychen651@gmail.com>
2024-12-07 16:55:46 +0200

    Profile page

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -94,24 +94,20 @@ export const updateProfile = async (req, res) => {
     const userId = req.user._id;
 
     if (!profilePic) {
-      return res.status(400).json({ message: "Profile picture is required" });
+      return res.status(400).json({ message: "Profile pic is required" });
     }
 
     const uploadResponse = await cloudinary.uploader.upload(profilePic);
-    const updateUser = await User.findByIdAndUpdate(
+    const updatedUser = await User.findByIdAndUpdate(
       userId,
       { profilePic: uploadResponse.secure_url },
       { new: true }
     );
-    res.status(200).json({
-      _id: updateUser._id,
-      fullName: updateUser.fullName,
-      email: updateUser.email,
-      profilePic: updateUser.profilePic,
-    });
+
+    res.status(200).json(updatedUser);
   } catch (error) {
-    console.log("Error in updateProfile controller", error.message);
-    res.status(500).json({ message: "Internal Server Error" });
+    console.log("error in update profile:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
