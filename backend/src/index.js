940e925c512f1abb83536f765fea2e86024208d2
roychen651 <roychen651@gmail.com>
2024-12-18 14:43:09 +0200

    project completed and ready for deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,8 +7,12 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
 
+import path from "path";
+
 dotenv.config();
 const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
 
 // Middleware
 app.use(express.json());
@@ -17,6 +21,14 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
